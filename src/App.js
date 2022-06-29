d396085377f7eadf043dec1ff75fb8c2d3230cfb
Jerika Kanz <jerikacapila@gmail.com>
2022-06-29 16:40:19 +0200

    Adding Drawer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+import logo from "./images/kanzept-logo.png";
 import Slides from "./components/Slides";
 import popularData from "./popularData";
 import veganData from "./veganData";
@@ -11,6 +12,11 @@ function App() {
   const [vegan, setVegan] = useState(veganData);
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState("");
+  const [isSearchShown, setIsSearchShown] = useState(false);
+
+  function showSearchBar() {
+    setIsSearchShown((prevShown) => !prevShown);
+  }
 
   const updateSearch = (e) => {
     setSearch(e.target.value);
@@ -53,18 +59,149 @@ function App() {
   });
 
   return (
-    <>
-      <Header />
-      <div className="carousel carousel-center rounded-box">
-        <h2 className="popular--title">Popular Recipes</h2>
-        {popularSlideElement}
+    <div class="drawer bg-white overflow-scroll">
+      <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+      <div class="drawer-content flex flex-col">
+        <div class="navbar bg-yellow-300">
+          <div class="navbar-start">
+            <label
+              for="my-drawer-3"
+              class="btn btn-ghost btn-square focus:bg-[#facc156b] active::bg-[#facc156b] hover:bg-[#facc156b]"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="fill-current h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="rgb(113 63 18)"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </label>
+          </div>
+
+          <div class="navbar-center">
+            {!isSearchShown && (
+              <a
+                class="btn btn-ghost normal-case text-xl text-yellow-900 font-cursive focus:bg-[#facc156b] active::bg-[#facc156b] hover:bg-[#facc156b]"
+                href="/"
+              >
+                Kanzepts{" "}
+                <img
+                  src={logo}
+                  alt=""
+                  className="app-header--logo w-5 max-h-full"
+                />
+              </a>
+            )}
+
+            {isSearchShown && (
+              <div class="form-control">
+                <div class="input-group">
+                  <button
+                    class="btn btn-sm btn-square bg-yellow-50 text-yellow-900 border-0 focus:bg-yellow-100 active:bg-yellow-100 h-10 text-lg px-5"
+                    onClick={showSearchBar}
+                  >
+                    ×
+                  </button>
+                  <input
+                    type="text"
+                    placeholder="Search Recipe"
+                    class="input-sm bg-yellow-50 text-yellow-900 placeholder-yellow-700 focus:outline-0 active:outline-0 w-52 h-10"
+                  />
+                  <button class="btn btn-sm btn-square bg-yellow-50 text-yellow-900 border-0 focus:bg-yellow-100 active:bg-yellow-100 h-10 text-lg px-5">
+                    🔍︎
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+          <div class="navbar-end">
+            {!isSearchShown && (
+              <button
+                class="btn btn-ghost btn-square focus:bg-[#facc156b] active::bg-[#facc156b] hover:bg-[#facc156b]"
+                onClick={showSearchBar}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="rgb(113 63 18)"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </button>
+            )}
+
+            <button class="btn btn-ghost btn-square focus:bg-[#facc156b] active:bg-[#facc156b] hover:bg-[#facc156b]">
+              <div class="indicator">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="rgb(113 63 18)"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                  />
+                </svg>
+                <span class="w-[0.1rem] h-[0.1rem] rounded-[50%] bg-amber-600 text-xs p-1 indicator-item"></span>
+              </div>
+            </button>
+          </div>
+        </div>
+
+        <div className="carousel carousel-center rounded-box">
+          <h2 className="popular--title">Popular Recipes</h2>
+          {popularSlideElement}
+        </div>
+
+        <div className="carousel carousel-center rounded-box">
+          <h2 className="popular--title">Veggies Recipes</h2>
+          {veganSlideElement}
+        </div>
       </div>
-      <div className="carousel carousel-center rounded-box">
-        <h2 className="popular--title">Veggies Recipes</h2>
-        {veganSlideElement}
+
+      <div class="drawer-side">
+        <label for="my-drawer-3" class="drawer-overlay"></label>
+        <ul class="menu p-4 overflow-y-auto w-48 bg-yellow-300 text-yellow-900">
+          <li>
+            <a href="/">Homepage</a>
+          </li>
+          <li>
+            <a href="/">Portfolio</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+        </ul>
       </div>
-      <Footer submit={getSearch} search={search} change={updateSearch} />
-    </>
+      {/* <Header />
+        <div className="carousel carousel-center rounded-box">
+          <h2 className="popular--title">Popular Recipes</h2>
+          {popularSlideElement}
+        </div>
+        <div className="carousel carousel-center rounded-box">
+          <h2 className="popular--title">Veggies Recipes</h2>
+          {veganSlideElement}
+        </div>
+        <Footer submit={getSearch} search={search} change={updateSearch} /> */}
+    </div>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,111 +1,266 @@
+import { useState } from "react";
 import logo from "../images/kanzept-logo.png";
-import "./Header.css";
 
 export default function Header() {
+  const [isSearchShown, setIsSearchShown] = useState(false);
+
+  function showSearchBar() {
+    setIsSearchShown((prevShown) => !prevShown);
+  }
+
   return (
-    <div class="navbar bg-yellow-300">
-      <div class="navbar-start">
-        <div class="dropdown">
-          <label
-            tabindex="0"
-            class="btn btn-ghost btn-square focus:bg-[#facc156b] active::bg-[#facc156b] hover:bg-[#facc156b] swap swap-rotate"
-          >
-            <input type="checkbox" />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="swap-off fill-current h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="rgb(113 63 18)"
+    <div class="drawer bg-white">
+      <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+      <div class="drawer-content flex flex-col">
+        <div class="navbar bg-yellow-300">
+          <div class="navbar-start">
+            <label
+              for="my-drawer-3"
+              class="btn btn-ghost btn-square focus:bg-[#facc156b] active::bg-[#facc156b] hover:bg-[#facc156b]"
             >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h7"
-              />
-            </svg>
-
-            <svg
-              class="swap-on fill-current text-yellow-900"
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 512 512"
-            >
-              <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
-            </svg>
-          </label>
-
-          <ul
-            tabindex="0"
-            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a href="/">Homepage</a>
-            </li>
-            <li>
-              <a href="/">Portfolio</a>
-            </li>
-            <li>
-              <a href="/">About</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="navbar-center">
-        <a
-          class="btn btn-ghost normal-case text-xl text-yellow-900 font-cursive focus:bg-[#facc156b] active::bg-[#facc156b] hover:bg-[#facc156b]"
-          href="/"
-        >
-          Kanzepts <img src={logo} alt="" className="app-header--logo" />
-        </a>
-      </div>
-      <div class="navbar-end">
-        <button class="btn btn-ghost btn-square focus:bg-[#facc156b] active::bg-[#facc156b] hover:bg-[#facc156b]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="rgb(113 63 18)"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </button>
-
-        <button class="btn btn-ghost btn-square focus:bg-[#facc156b] active::bg-[#facc156b] hover:bg-[#facc156b]">
-          <div class="indicator">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="rgb(113 63 18)"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-              />
-            </svg>
-            <span class="w-[0.1rem] h-[0.1rem] rounded-[50%] bg-yellow-800 text-[0.5rem] text-white p-1 indicator-item"></span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="fill-current h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="rgb(113 63 18)"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </label>
           </div>
-        </button>
+
+          <div class="navbar-center">
+            {!isSearchShown && (
+              <a
+                class="btn btn-ghost normal-case text-xl text-yellow-900 font-cursive focus:bg-[#facc156b] active::bg-[#facc156b] hover:bg-[#facc156b]"
+                href="/"
+              >
+                Kanzepts{" "}
+                <img
+                  src={logo}
+                  alt=""
+                  className="app-header--logo w-5 max-h-full"
+                />
+              </a>
+            )}
+
+            {isSearchShown && (
+              <div class="form-control">
+                <div class="input-group">
+                  <button
+                    class="btn btn-sm btn-square bg-yellow-50 text-yellow-900 border-0 focus:bg-yellow-100 active:bg-yellow-100 h-10 text-lg px-5"
+                    onClick={showSearchBar}
+                  >
+                    ×
+                  </button>
+                  <input
+                    type="text"
+                    placeholder="Search Recipe"
+                    class="input-sm bg-yellow-50 text-yellow-900 placeholder-yellow-700 focus:outline-0 active:outline-0 w-52 h-10"
+                  />
+                  <button class="btn btn-sm btn-square bg-yellow-50 text-yellow-900 border-0 focus:bg-yellow-100 active:bg-yellow-100 h-10 text-lg px-5">
+                    🔍︎
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+          <div class="navbar-end">
+            {!isSearchShown && (
+              <button
+                class="btn btn-ghost btn-square focus:bg-[#facc156b] active::bg-[#facc156b] hover:bg-[#facc156b]"
+                onClick={showSearchBar}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="rgb(113 63 18)"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </button>
+            )}
+
+            <button class="btn btn-ghost btn-square focus:bg-[#facc156b] active:bg-[#facc156b] hover:bg-[#facc156b]">
+              <div class="indicator">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="rgb(113 63 18)"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                  />
+                </svg>
+                <span class="w-[0.1rem] h-[0.1rem] rounded-[50%] bg-amber-600 text-xs p-1 indicator-item"></span>
+              </div>
+            </button>
+          </div>
+        </div>
+        Content
+      </div>
+      <div class="drawer-side">
+        <label for="my-drawer-3" class="drawer-overlay"></label>
+        <ul class="menu p-4 overflow-y-auto w-48 bg-yellow-300 text-yellow-900">
+          <li>
+            <a href="/">Homepage</a>
+          </li>
+          <li>
+            <a href="/">Portfolio</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+        </ul>
       </div>
     </div>
 
-    // <div className="app-header--container">
-    //   <h1 className="app-header--title">
-    //     Kanzepts
-    //     <img src={logo} alt="" className="app-header--logo" />
-    //   </h1>
+    // <div class="navbar bg-yellow-300">
+    //   <div class="navbar-start">
+    //     <div class="dropdown">
+    //       <label
+    //         tabindex="0"
+    //         class="btn btn-ghost btn-square focus:bg-[#facc156b] active::bg-[#facc156b] hover:bg-[#facc156b] swap swap-rotate"
+    //       >
+    //         <input type="checkbox" />
+    //         <svg
+    //           xmlns="http://www.w3.org/2000/svg"
+    //           class="swap-off fill-current h-5 w-5"
+    //           fill="none"
+    //           viewBox="0 0 24 24"
+    //           stroke="rgb(113 63 18)"
+    //         >
+    //           <path
+    //             stroke-linecap="round"
+    //             stroke-linejoin="round"
+    //             stroke-width="2"
+    //             d="M4 6h16M4 12h16M4 18h7"
+    //           />
+    //         </svg>
+
+    //         <p
+    //           class="swap-on fill-current text-yellow-900 text-2xl"
+    //           // onClick={showDropDownBar}
+    //         >
+    //           ×
+    //         </p>
+    //       </label>
+
+    //       <ul
+    //         tabindex="0"
+    //         class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+    //       >
+    // <li>
+    //   <a href="/">Homepage</a>
+    // </li>
+    // <li>
+    //   <a href="/">Portfolio</a>
+    // </li>
+    // <li>
+    //   <a href="/">About</a>
+    // </li>
+    //       </ul>
+    //     </div>
+    //   </div>
+
+    // <div class="navbar-center">
+    //   {!isSearchShown && (
+    //     <a
+    //       class="btn btn-ghost normal-case text-xl text-yellow-900 font-cursive focus:bg-[#facc156b] active::bg-[#facc156b] hover:bg-[#facc156b]"
+    //       href="/"
+    //     >
+    //       Kanzepts{" "}
+    //       <img
+    //         src={logo}
+    //         alt=""
+    //         className="app-header--logo w-5 max-h-full"
+    //       />
+    //     </a>
+    //   )}
+
+    //   {isSearchShown && (
+    //     <div class="form-control">
+    //       <div class="input-group">
+    //         <button
+    //           class="btn btn-sm btn-square bg-yellow-50 text-yellow-900 border-0 focus:bg-yellow-100 active:bg-yellow-100 h-10 text-lg px-5"
+    //           onClick={showSearchBar}
+    //         >
+    //           ×
+    //         </button>
+    //         <input
+    //           type="text"
+    //           placeholder="Search Recipe"
+    //           class="input-sm bg-yellow-50 text-yellow-900 placeholder-yellow-700 focus:outline-0 active:outline-0 w-52 h-10"
+    //         />
+    //         <button class="btn btn-sm btn-square bg-yellow-50 text-yellow-900 border-0 focus:bg-yellow-100 active:bg-yellow-100 h-10 text-lg px-5">
+    //           🔍︎
+    //         </button>
+    //       </div>
+    //     </div>
+    //   )}
+    // </div>
+
+    // <div class="navbar-end">
+    //   {!isSearchShown && (
+    //     <button
+    //       class="btn btn-ghost btn-square focus:bg-[#facc156b] active::bg-[#facc156b] hover:bg-[#facc156b]"
+    //       onClick={showSearchBar}
+    //     >
+    //       <svg
+    //         xmlns="http://www.w3.org/2000/svg"
+    //         class="h-5 w-5"
+    //         fill="none"
+    //         viewBox="0 0 24 24"
+    //         stroke="rgb(113 63 18)"
+    //       >
+    //         <path
+    //           stroke-linecap="round"
+    //           stroke-linejoin="round"
+    //           stroke-width="2"
+    //           d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+    //         />
+    //       </svg>
+    //     </button>
+    //   )}
+
+    //   <button class="btn btn-ghost btn-square focus:bg-[#facc156b] active:bg-[#facc156b] hover:bg-[#facc156b]">
+    //     <div class="indicator">
+    //       <svg
+    //         xmlns="http://www.w3.org/2000/svg"
+    //         class="h-5 w-5"
+    //         fill="none"
+    //         viewBox="0 0 24 24"
+    //         stroke="rgb(113 63 18)"
+    //       >
+    //         <path
+    //           stroke-linecap="round"
+    //           stroke-linejoin="round"
+    //           stroke-width="2"
+    //           d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+    //         />
+    //       </svg>
+    //       <span class="w-[0.1rem] h-[0.1rem] rounded-[50%] bg-amber-600 text-xs p-1 indicator-item"></span>
+    //     </div>
+    //   </button>
+    // </div>
     // </div>
   );
 }
